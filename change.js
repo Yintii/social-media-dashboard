e09393e1d39942908a3b278e6b0e9dc1399352c3
yintii <kheart@keleheart.com>
2020-06-13 10:29:58 -0500

    I have added the styles for the light version of the website, and I currently making the javascript file to control toggling between the two

---
+++ change.js
@@ -0,0 +1,15 @@
+var bod = document.getElementsByName("body");
+var headerText = document.getElementsById("headerText");
+var headerToggle = document.getElementById("headerToggle");
+var toggler = document.getElementById("toggler");
+var boggle = document.getElementById("boggle");
+var followItems = document.getElementsByClassName("follow-item");
+var overviewItems = document.getElementsByClassName("overview-items");
+
+var elements = [bod, headerText, headerToggle,
+                toggler, boggle, followItems,
+                overviewItems];
+
+function changeTheme(){
+    
+}
