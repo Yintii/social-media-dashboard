6c691db4905de8da6b68957fcb2425f524d996ad
yintii <kheart@keleheart.com>
2020-06-13 12:49:33 -0500

    cannot get the JS to work for the toggle

--- change.js
+++ change.js
@@ -1,15 +1,22 @@
 var bod = document.getElementsByName("body");
-var headerText = document.getElementsById("headerText");
+var headerText = document.getElementById("headerText");
 var headerToggle = document.getElementById("headerToggle");
 var toggler = document.getElementById("toggler");
 var boggle = document.getElementById("boggle");
 var followItems = document.getElementsByClassName("follow-item");
 var overviewItems = document.getElementsByClassName("overview-items");
 
-var elements = [bod, headerText, headerToggle,
-                toggler, boggle, followItems,
-                overviewItems];
+
 
 function changeTheme(){
+    var elements = [bod, headerText, headerToggle,
+        toggler, boggle, followItems, overviewItems];
     
+    elements.forEach((ele)=>{
+        if(ele.classList.contains("light")){
+            ele.classList.remove("light");
+        }else if(ele.classList.contains("light") == false){
+            ele.classList.add("light");
+        }
+    })
 }
